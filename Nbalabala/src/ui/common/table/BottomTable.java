181f258d370f16a4621b451581798c4d8b9f3431
陈云龙 <cyl19941016@sina.com>
2015-03-21 18:39:43 +0800

    表头不移动问题已经解决【昨天忘记传了】

--- Nbalabala/src/ui/common/table/BottomTable.java
+++ Nbalabala/src/ui/common/table/BottomTable.java
@@ -29,7 +29,7 @@ public class BottomTable extends JTable {
 	protected String[] columnNames = null;
 	protected Object[][] rowData = null;
 
-	public BottomTable(String[] columnNames, Object[][] rowData) {
+	public BottomTable(Object[][] rowData, String[] columnNames) {
 		super(rowData, columnNames);
 		this.columnNames = columnNames;
 		this.rowData = rowData;
@@ -43,14 +43,14 @@ public class BottomTable extends JTable {
 		return false;
 	} // 禁止编辑单元格
 
-	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
-		g.setColor(UIConfig.TABLE_BORDER_COLOR);
-		g.drawLine(0, getHeight() - 1, getWidth(), getHeight() - 1);
-		g.drawLine(getWidth() - 1, 0, getWidth() - 1, getHeight() - 1);
-		g.drawLine(0, 0, 0, getHeight());
-	}
+	// @Override
+//	public void paint(Graphics g) {
+//		super.paint(g);
+//		g.setColor(UIConfig.TABLE_BORDER_COLOR);
+//		g.drawLine(0, getHeight() - 1, getWidth(), getHeight() - 1);
+//		g.drawLine(getWidth() - 1, 0, getWidth() - 1, getHeight() - 1);
+//		g.drawLine(0, 0, 0, getHeight());
+//	}
 
 	/**
 	 * 装饰Table
@@ -58,46 +58,46 @@ public class BottomTable extends JTable {
 	 * @version 2015年3月20日  下午6:32:45
 	 */
 	public void decorateTable() {
-		this.setFont(UIConfig.TABLE_FONT);	// 设置表格内容字体
-		this.setForeground(UIConfig.TABLE_FORE_COLOR);	// 表格前景色
-		this.setSelectionForeground(UIConfig.TABLE_SELECTIONFORE);	// 选择文本的前景色
-		this.setBorder(null);
-		this.setRowHeight(rowHeight);	// 每一行的高
-		this.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		// this.setFont(UIConfig.TABLE_FONT);	// 设置表格内容字体
+		// this.setForeground(UIConfig.TABLE_FORE_COLOR);	// 表格前景色
+		// this.setSelectionForeground(UIConfig.TABLE_SELECTIONFORE);	// 选择文本的前景色
+		// this.setBorder(null);
+		// this.setRowHeight(rowHeight);	// 每一行的高
+		// this.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		//	infoTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		this.setIntercellSpacing(new Dimension(0, 0));
+		// this.setIntercellSpacing(new Dimension(0, 0));
 		// 将表格设置为透明，表格同样包括表格本身和其中的内容项 仅仅将表格本身设置为透明也没有用，应该将其中的内容项也设置为透明
 		// 内容项的设置是通过设置渲染器的透明来实现
-		this.setOpaque(false);
-		this.setGridColor(UIConfig.TABLE_BORDER_COLOR);	// 表格边框的颜色
-		DefaultTableCellRenderer render = new DefaultTableCellRenderer();
-		render.setOpaque(false); // 将渲染器设置为透明
-		render.setHorizontalAlignment(JLabel.CENTER);	// 设置表格中内容居中显示
+		// this.setOpaque(false);
+		// this.setGridColor(UIConfig.TABLE_BORDER_COLOR);	// 表格边框的颜色
+		// DefaultTableCellRenderer render = new DefaultTableCellRenderer();
+		// render.setOpaque(false); // 将渲染器设置为透明
+		// render.setHorizontalAlignment(JLabel.CENTER);	// 设置表格中内容居中显示
 		// 将这个渲染器设置到table里。
 		// 这个设置在没有另外专门对column设置的情况下有效
 		// 若你对某个column特殊指定了渲染器，则对于这个column，它将不调用render渲染器
 		// 因此为了保证透明，如果你对column额外指定了渲染器，那么在额外的渲染器里也应该设置透明
-		this.setDefaultRenderer(Object.class, render);
+		// this.setDefaultRenderer(Object.class, render);
 		// 设置显示范围
-		Dimension viewSize = new Dimension();
-		viewSize.width = this.getColumnModel().getTotalColumnWidth();
-		viewSize.height = 10 * this.getRowHeight();
-		this.setPreferredScrollableViewportSize(viewSize);
+//		Dimension viewSize = new Dimension();
+//		viewSize.width = this.getColumnModel().getTotalColumnWidth();
+//		viewSize.height = 10 * this.getRowHeight();
+//		this.setPreferredScrollableViewportSize(viewSize);
 		// 设置头部透明
 		// 头部实际上也是一个JTABLE，只有一行而已。
-		JTableHeader header = this.getTableHeader();// 获取头部
-		header.setPreferredSize(new Dimension(30, rowHeight));
-		header.setOpaque(false); // 设置头部为透明
-		header.setBackground(UIConfig.TABLE_HEADER_BACK_COLOR);	// 设置头部背景色
-		header.getTable().setOpaque(false);// 设置头部里面的表格透明
-		// 头部的表格也像前面的表格设置一样，还需要将里面的单元项设置为透明 因此同样需要对头部单元项进行透明度设置，这里还是用渲染器。
-		// 但这里有个问题就是，若将头部渲染器直接像上文一样设置，则它的下面没有横线 因此，我们需要一个专用的头部渲染器来手动画横线
-		header.setDefaultRenderer(new HeaderCellRenderer());
-		TableCellRenderer headerRenderer = header.getDefaultRenderer();
-		if (headerRenderer instanceof JLabel) {
-			((JLabel)headerRenderer).setHorizontalAlignment(JLabel.CENTER);
-			((JLabel)headerRenderer).setOpaque(false);
-		}
+//		JTableHeader header = this.getTableHeader();// 获取头部
+//		header.setPreferredSize(new Dimension(30, rowHeight));
+//		header.setOpaque(false); // 设置头部为透明
+//		header.setBackground(UIConfig.TABLE_HEADER_BACK_COLOR);	// 设置头部背景色
+//		header.getTable().setOpaque(false);// 设置头部里面的表格透明
+//		// 头部的表格也像前面的表格设置一样，还需要将里面的单元项设置为透明 因此同样需要对头部单元项进行透明度设置，这里还是用渲染器。
+//		// 但这里有个问题就是，若将头部渲染器直接像上文一样设置，则它的下面没有横线 因此，我们需要一个专用的头部渲染器来手动画横线
+//		header.setDefaultRenderer(new HeaderCellRenderer());
+//		TableCellRenderer headerRenderer = header.getDefaultRenderer();
+//		if (headerRenderer instanceof JLabel) {
+//			((JLabel)headerRenderer).setHorizontalAlignment(JLabel.CENTER);
+//			((JLabel)headerRenderer).setOpaque(false);
+//		}
 
 	}
 
--- Nbalabala/src/ui/panel/teamdata/TeamDataPanel.java
+++ Nbalabala/src/ui/panel/teamdata/TeamDataPanel.java
@@ -1,16 +1,20 @@
 package ui.panel.teamdata;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 import ui.UIConfig;
 import ui.common.button.ImgButton;
 import ui.common.button.TextButton;
 import ui.common.panel.BottomPanel;
-import ui.common.table.BottomScrollPane;
-import ui.common.table.BottomTable;
 import ui.controller.MainController;
 import ui.panel.playerData.Line_4_Button;
 import bl.teamseasonbl.TeamSeasonAnalysis;
@@ -32,7 +36,7 @@ public class TeamDataPanel extends BottomPanel {
 	private static final long serialVersionUID = -4296014620804951285L;
 
 	/** 队伍数据表格 */
-	private BottomTable teamDataTable;
+	private JTable teamDataTable;
 
 	/** 条件按钮 */
 	SelectButton[] button;
@@ -191,16 +195,72 @@ public class TeamDataPanel extends BottomPanel {
 	/**
 	 * 添加当前登录的用户的信息table
 	 * @author cylong
-	 * @version 2014年12月15日 下午10:40:36
+	 * @version 2015年3月20日  下午6:37:48
 	 */
 	private void addTeamDataTable() {
-		String[] columnNames = {"球队名称1", "球队名称2", "球队名称3", "球队名称4", "球队名称5", "球队名称6", "球队名称7", "球队名称8", "球队名称9",
+		String[] columnNames = {"名称", "胜", "负", "总", "命中", "球队名称6", "球队名称7", "球队名称8", "球队名称9",
 									"球队名称10", "球队名称11", "球队名称22", "球队名称33", "球队名称44", "球队名称55", "球队名称66", "球队名称", "球队名称"};
-		String[][] rowData = new String[10][columnNames.length];
-		teamDataTable = new BottomTable(columnNames, rowData);
-		BottomScrollPane scroll = new BottomScrollPane(teamDataTable);
+		String[][] rowData = new String[20][columnNames.length];
+		rowData[0][0] = "2333";
+		
+//		teamDataTable = new JTable(rowData, columnNames);
+//		// teamDataTable.setSize(new Dimension(1000, 1000));
+//		
+//		JPanel panel = new JPanel();
+//		panel.add(teamDataTable);
+//		panel.setSize(2000, 2000);
+//		BottomScrollPane scroll = new BottomScrollPane(panel);
+//		scroll.setLocation(57, 239);
+//		this.add(scroll);
+		
+		JTable table = new JTable(rowData, columnNames);
+		table.setSize(1500, 1500);
+		table.setLocation(57, 239);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+//		table.getTableHeader().setPreferredSize(new Dimension(30, 30));
+	
+		scroll = new JScrollPane(table);
+		scroll.setSize(888, 290);
 		scroll.setLocation(57, 239);
+		// scroll.setLayout(null);
+		// scroll.setOpaque(false);
 		this.add(scroll);
+		
+		JButton button = new JButton("换页");
+		button.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				String[] columnNames = {"名称1", "胜1", "负1", "总1", "命中1", "球队名称6", "球队名称7", "球队名称8", "球队名称9",
+										"球队名称10", "球队名称11", "球队名称22", "球队名称33", "球队名称44", "球队名称55", "球队名称66", "球队名称", "球队名称"};
+				String[][] rowData = new String[20][columnNames.length];
+				
+				JTable table = new JTable(rowData, columnNames);
+				table.setSize(1500, 1500);
+				table.setLocation(57, 239);
+				table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+				table.getTableHeader().setPreferredSize(new Dimension(30, 30));
+			
+				JPanel panel = new JPanel();
+				panel.setPreferredSize(new Dimension(2000, 2000));
+				panel.setLocation(50, 50);
+				panel.setBackground(Color.BLUE);
+				panel.setLayout(null);
+				panel.add(table);
+				
+				int value = scroll.getVerticalScrollBar().getValue();
+				TeamDataPanel.this.remove(scroll);
+				scroll = new JScrollPane(table);
+				scroll.setSize(888, 290);
+				scroll.setLocation(57, 239);
+				scroll.getVerticalScrollBar().setValue(value);
+				System.out.println(value);
+ 				TeamDataPanel.this.add(scroll);
+			}
+		});
+		button.setSize(100, 30);
+		button.setLocation(500, 20);
+		this.add(button);
 	}
+	
+	JScrollPane scroll;
 
 }
