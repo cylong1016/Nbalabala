0f0dd6d79c467c34e8ec7dae046588153c599091
IssacDing <IssacDing@yahoo.com>
2015-04-08 20:16:49 +0800

    热点模块blservice写好了

---
+++ Nbalabala/src/blservice/HotBLService.java
@@ -0,0 +1,33 @@
+package blservice;
+
+import java.util.ArrayList;
+
+import enums.HotFastestPlayerProperty;
+import enums.HotSeasonPlayerProperty;
+import enums.HotSeasonTeamProperty;
+import enums.HotTodayPlayerProperty;
+import vo.HotFastestPlayerVO;
+import vo.HotSeasonPlayerVO;
+import vo.HotSeasonTeamVO;
+import vo.HotTodayPlayerVO;
+
+/**
+ * 热点功能模块所需接口
+ * @author Issac Ding
+ * @version 2015年4月8日  下午6:59:35
+ */
+public interface HotBLService {
+	
+	/** 根据筛选指标返回该指标的当日前5名球员 */
+	public ArrayList<HotTodayPlayerVO> getHotTodayPlayers(HotTodayPlayerProperty property);
+	
+	/** 赛季热点球员 */
+	public ArrayList<HotSeasonPlayerVO> getHotSeasonPlayers(HotSeasonPlayerProperty property);
+	
+	/** 赛季热点球队 */
+	public ArrayList<HotSeasonTeamVO> getHotSeasonTeams(HotSeasonTeamProperty property);
+	
+	/** 进步最快球员 */
+	public ArrayList<HotFastestPlayerVO> getHotFastestPlayers(HotFastestPlayerProperty property);
+	
+}
