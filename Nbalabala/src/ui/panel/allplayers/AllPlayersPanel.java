88c299a14cd00a3a5a725ab406f81c4c3fdc8ba5
lsy <864986781@qq.com>
2015-04-14 21:13:57 +0800

    修改了一下全部球员界面的表格

--- Nbalabala/src/ui/panel/allplayers/AllPlayersPanel.java
+++ Nbalabala/src/ui/panel/allplayers/AllPlayersPanel.java
@@ -2,18 +2,26 @@ package ui.panel.allplayers;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 
 import ui.UIConfig;
+import ui.common.UserMouseAdapter;
 import ui.common.button.ImgButton;
 import ui.common.panel.BottomPanel;
+import ui.common.table.BottomScrollPane;
+import ui.common.table.BottomTable;
 import ui.common.textField.MyTextField;
+import ui.controller.MainController;
+import utility.Constants;
 import vo.PlayerProfileVO;
 import bl.playerquerybl.PlayerQuery;
 import blservice.PlayerQueryBLService;
@@ -46,8 +54,15 @@ public class AllPlayersPanel extends BottomPanel {
 	private MyTextField queryTextField;
 	
 	private PlayerQueryBLService playerInfo = new PlayerQuery();
-	/** 球员信息界面 */
-	PlayerTable playerTable;
+	
+	private static final String[]COLUMN_NAMES = {"球员头像","英文名", "所属球队", "球衣号码", "位置", "生日" };
+	public static BottomScrollPane SCROLL;
+	private BottomTable table;
+	/** 头像宽度 */
+	private static final int PORTRAIT_WIDTH = 70;
+	ArrayList<PlayerProfileVO> players;
+	Object [][] rowData;
+	int size,lth;
 	
 	/**
 	 * @param url 背景图片的Url
@@ -60,15 +75,80 @@ public class AllPlayersPanel extends BottomPanel {
 		addTextField();
 		iniSet();		//一开始默认A选中
 		addListener();
-		ArrayList<PlayerProfileVO> playerInfoArr = playerInfo.getPlayerProfileByInitial('A');
-		playerTable  = new PlayerTable(playerInfoArr,this);
+		players = playerInfo.getPlayerProfileByInitial('A');
+		addTable();
+		setTable();
 	}
 	
 	public void refresh(){
-		ArrayList<PlayerProfileVO> playerInfoArr = playerInfo.getPlayerProfileByInitial(LetterButton.current.letter);
-		this.remove(PlayerTable.SCROLL); 
-		playerTable = new PlayerTable(playerInfoArr,this);
+		players= playerInfo.getPlayerProfileByInitial(LetterButton.current.letter);
+		setTable();
 		AllPlayersPanel.this.repaint();
+	}
+	
+	public void addTable() {
+		size = players.size();
+		lth = COLUMN_NAMES.length;
+		rowData = new String[size][lth];
+		table = new BottomTable(rowData, COLUMN_NAMES);
+		try {
+			table.addMouseListener(new UserMouseAdapter() {
+				
+				public void mouseClicked(MouseEvent e) {
+					if (e.getClickCount() < 2)
+						return;
+					int rowI = table.rowAtPoint(e.getPoint());// 得到table的行号
+					if (rowI > -1) {
+						MainController.toPlayerInfoPanel(AllPlayersPanel.this, players.get(rowI).getName(), AllPlayersPanel.this);
+					}
+					
+				}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		table.setRowHeight(57);
+		table.setWidth(new int[] { 123, 200, 150, 104, 89, 116 });
+		table.setForeground(Color.WHITE);
+		table.cancelVerticalLines();
+		table.setRealOpaque();
+		
+		SCROLL = new BottomScrollPane(table);
+		SCROLL.setBounds(101, 160, 802, 365);
+		SCROLL.cancelBgImage();
+		this.add(SCROLL);
+	}
+	
+	public void setTable() {
+		size = players.size();
+		lth = COLUMN_NAMES.length;
+		rowData = new String[size][lth];
+		ArrayList<ImageIcon> iconArr = new ArrayList<ImageIcon>();
+		table.setModel(new DefaultTableModel(rowData,COLUMN_NAMES));
+		table.setRowHeight(57);
+		table.setWidth(new int[] { 123, 200, 150, 104, 89, 116 });
+		table.setForeground(Color.WHITE);
+		table.cancelVerticalLines();
+		table.setRealOpaque();
+		
+		for (int i = 0; i < size; i++) {
+			PlayerProfileVO ppVO = players.get(i);
+			Image protrait = ppVO.getPortrait();
+			int height = protrait.getHeight(null) * 70 / protrait.getWidth(null);// 按比例，将高度缩减
+			Image smallImg = protrait.getScaledInstance(PORTRAIT_WIDTH, height, Image.SCALE_SMOOTH);
+			ImageIcon ic = new ImageIcon(smallImg);
+			iconArr.add(ic);
+			table.setValueAt(ppVO.getName(), i, 1);
+			table.setValueAt(Constants.translateTeamAbbr(ppVO.getTeam()), i, 2);
+			table.setValueAt(ppVO.getNumber(), i, 3);
+			table.setValueAt(ppVO.getPosition(), i, 4);
+			table.setValueAt( ppVO.getBirth(), i, 5);
+			
+		}
+		MyTableCellRenderer myRenderer = new MyTableCellRenderer();
+		myRenderer.icon = iconArr;
+		// iconArr.clear();
+		table.getColumnModel().getColumn(0).setCellRenderer(myRenderer);
 	}
 	
 	/**
@@ -82,9 +162,8 @@ public class AllPlayersPanel extends BottomPanel {
 		this.add(queryButton);
 		queryButton.addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent e) {
-				ArrayList<PlayerProfileVO> playerInfoArr = playerInfo.searchPlayers(queryTextField.getText());
-				AllPlayersPanel.this.remove(PlayerTable.SCROLL);
-				playerTable = new PlayerTable(playerInfoArr,AllPlayersPanel.this);
+				players= playerInfo.searchPlayers(queryTextField.getText());
+				setTable();
 				AllPlayersPanel.this.repaint();
 			}
 		});
@@ -130,9 +209,8 @@ public class AllPlayersPanel extends BottomPanel {
 			LetterButton.current.back();
 			LetterButton.current = (LetterButton) e.getSource();
 			char goal = LetterButton.current.letter;
-			ArrayList<PlayerProfileVO> vo = playerInfo.getPlayerProfileByInitial(goal);
-			AllPlayersPanel.this.remove(PlayerTable.SCROLL);
-			playerTable  = new PlayerTable(vo,AllPlayersPanel.this);
+		    players = playerInfo.getPlayerProfileByInitial(goal);
+		    setTable();
 		}
 		
 	}
