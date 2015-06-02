ebc723e11b9d0538ff3382008ec89d6e27a6ba89
clyong1016 <cyl19941016@sina.com>
2015-06-03 00:55:43 +0800

    按照标准修改了变量名和数据库中的命名

--- CaptureNBAData/src/capture/Match.java
+++ CaptureNBAData/src/capture/Match.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,29 +22,35 @@ import mysql.MySQL;
  * @author cylong
  * @version 2015年5月12日  下午7:54:54
  */
-public class Season implements NBAData {
+public class Match implements NBAData {
 	
 	public static void main(String[] args) {
-		Season s = new Season();
+		Match s = new Match();
 		s.capture();
 	}
 	
 	/** 网站链接 */ 
 	private String root = "http://www.basketball-reference.com";
 	/** 赛季数据的url */
-    private String seasonUrl;
+    private String matchUrl;
     private MySQL mysql;
     private Connection mysqlConn;
     /** 最近多少年的数据 */
     private int maxYear = 40;
     /** 比赛ID */
-    private int gameID = 1;
+    private int matchID = 1;
+    /* 将英文转换为数字 */
+    private Hashtable<String, String> month = new Hashtable<String, String>(12);
+    private String[] monthStrArr = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+                                 "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
     
-    
-    public Season() {
-    	this.seasonUrl = root + "/leagues";
+    public Match() {
+    	this.matchUrl = root + "/leagues";
     	mysql = new MySQL();
     	mysqlConn = mysql.getConn(); // 获得数据库连接
+    	for(int i = 0; i < monthStrArr.length; i++) {
+    		month.put(monthStrArr[i], String.valueOf(i + 1));
+		}
     }
     
     /**
@@ -60,10 +67,10 @@ public class Season implements NBAData {
      * @author cylong
      * @version 2015年5月19日  下午8:20:07
      */
-    private void captureSeasonData() {
+    private void captureMatchData() {
     	InputStream input = null;
     	BufferedReader reader = null;
-    	HttpURLConnection urlConn = getConn(seasonUrl);
+    	HttpURLConnection urlConn = getConn(matchUrl);
     	int i = 1; // 记录最近多少年
     	try {
     		input = urlConn.getInputStream();
@@ -132,7 +139,7 @@ public class Season implements NBAData {
 							hasDetalData = false;
 						}
 						cellData.add(href);
-						System.out.println(href + "， gameID: " + gameID);
+						System.out.println(href + "， gameID: " + matchID);
 					} else {
 						cellData.add(info);
 					}
@@ -140,8 +147,8 @@ public class Season implements NBAData {
 					if(i == 0) { // 一行数据读取完毕
 						// 存入数据库
 						if(hasDetalData) { // 有详细信息再存入数据库
-							insertIntoTeamSeason(cellData, regularOrPlayoff); // 存储球队比赛数据
-							gameID++;
+							insertIntoMatchProfile(cellData, regularOrPlayoff); // 存储球队比赛数据
+							matchID++;
 						}
 						hasDetalData = true;
 						i = culumnNum;
@@ -226,7 +233,7 @@ public class Season implements NBAData {
 					break;
 				}
 			}
-			capturePlayerSeason(reader, returnList.get(1), returnList.get(6)); // 继续读取球员的比赛信息
+			capturePlayerMatch(reader, returnList.get(1), returnList.get(6)); // 继续读取球员的比赛信息
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
@@ -248,15 +255,15 @@ public class Season implements NBAData {
 	 * @param home 主场球队名缩写
      * @version 2015年5月28日  下午3:45:12
      */
-	private void capturePlayerSeason(BufferedReader reader, String visitor, String home) {
+	private void capturePlayerMatch(BufferedReader reader, String visitor, String home) {
     	String BASIC_BOX_SCORE_STATS = "Basic Box Score Stats"; // 球员比赛数据表格的表头
-    	int homeOrVisitor = 0; // 0:客场、1:主场
+    	char homeOrRoad = 'R'; // R:客场、H:主场
     	String reserves = "Reserves"; // 候补球员的表头
     	try {
 			String temp = null;
 			while((temp = reader.readLine()) != null) {
 				// 球员的比赛数据
-				ArrayList<String> playerSeason = new ArrayList<String>();
+				ArrayList<String> playerMatch = new ArrayList<String>();
 				// 找到球员比赛信息的第一个表【客场】
 				if(Pattern.matches(".*<th.*>" + BASIC_BOX_SCORE_STATS + "</th>", temp)) {
 					int isStarter = 1; // 是否先发：1是、0不是
@@ -269,27 +276,27 @@ public class Season implements NBAData {
 						Matcher matcherPlayerData = patternPlayerData.matcher(temp);
 						if(matcherPlayerData.find()) {
 							String playerData = matcherPlayerData.group("playerData");
-							playerSeason.add(playerData);
+							playerMatch.add(playerData);
 						}
 						if(Pattern.matches("</tr>", temp)) { // 一个球员的数据读取完毕
-							playerSeason.add(String.valueOf(homeOrVisitor)); // 主场或者客场
-							playerSeason.add(String.valueOf(isStarter)); // 是否先发
-							if(homeOrVisitor == 0) {
-								playerSeason.add(visitor);
+							playerMatch.add(String.valueOf(homeOrRoad)); // 主场或者客场
+							playerMatch.add(String.valueOf(isStarter)); // 是否先发
+							if(homeOrRoad == 0) {
+								playerMatch.add(visitor);
 							} else {
-								playerSeason.add(home);
+								playerMatch.add(home);
 							}
 							// 球员数据为24个，少于24个数据的就是没有记录
 							// 表格最后一行是Team Totals，不记录
-							if(playerSeason.size() == 24) {
+							if(playerMatch.size() == 24) {
 								// 球员比赛信息表的最后一行，接着读取下一个Basic Box Score Stats表
-								if(playerSeason.get(0).equals("Team Totals")) {
-									homeOrVisitor = 1; // 下一个表是主场
+								if(playerMatch.get(0).equals("Team Totals")) {
+									homeOrRoad = 'H'; // 下一个表是主场
 									break; // 内层的while循环
 								}
-								insertIntoPlayerSeason(playerSeason);
+								insertIntoPlayerMatch(playerMatch);
 							}
-							playerSeason.clear();
+							playerMatch.clear();
 						}
 					}
 				}
@@ -306,72 +313,72 @@ public class Season implements NBAData {
 	}
 
 	/**
-	 * 将球员比赛数据插入到数据库中
-	 * @param playerSeason
+	 * 将球员比赛数据插入到数据库match_player表中
+	 * @param playerMatch
 	 * @author cylong
 	 * @version 2015年5月28日  下午5:00:06
 	 */
-	private void insertIntoPlayerSeason(ArrayList<String> playerSeason) {
-		for(int i = 0; i < playerSeason.size(); i++) {
+	private void insertIntoPlayerMatch(ArrayList<String> playerMatch) {
+		for(int i = 0; i < playerMatch.size(); i++) {
 			// 有些数据网站上是空的，存到数据库中为null
-			if(playerSeason.get(i).equals("")) {
-				playerSeason.set(i, "0");
+			if(playerMatch.get(i).equals("")) {
+				playerMatch.set(i, "0");
 			}
 		}
-		String sql = "INSERT INTO player_season ("
-				+ "gameID, "
-				+ "home_or_visitor, "
-				+ "team, "
+		String sql = "INSERT INTO match_player ("
+				+ "match_id, "
+				+ "home_or_road, "
+				+ "team_abbr, "
 				+ "player_name, "
 				+ "is_starter, "
-				+ "minutes_played, "
-				+ "field_goals, "
-				+ "field_goals_attempts, "
-				+ "field_goals_percentage, "
-				+ "three_field_goals, "
-				+ "three_field_goals_attempts, "
-				+ "three_field_goals_percentage, "
-				+ "free_throws, "
-				+ "free_throws_attempts, "
-				+ "free_throws_percentage, "
-				+ "offensive_rebounds, "
-				+ "defensive_rebounds, "
-				+ "total_rebounds, "
-				+ "assists, "
-				+ "steals, "
-				+ "blocks, "
-				+ "turnovers, "
-				+ "personal_fouls, "
-				+ "points, "
+				+ "time_played, "
+				+ "field_made, "
+				+ "field_attempt, "
+				+ "field_percent, "
+				+ "threepoint_made, "
+				+ "threepoint_attempt, "
+				+ "threepoint_percent, "
+				+ "freethrow_made, "
+				+ "freethrow_attempt, "
+				+ "freethrow_percent, "
+				+ "offensive_rebound, "
+				+ "defensive_rebound, "
+				+ "total_rebound, "
+				+ "assist, "
+				+ "steal, "
+				+ "block, "
+				+ "turnover, "
+				+ "foul, "
+				+ "score, "
 				+ "plus_minus"
 				+ ") VALUES ("
-					+ gameID + ", "
-					+ playerSeason.get(21) + ", "
-					+ "'" + playerSeason.get(23) + "', "
-					+ "'" + playerSeason.get(0).replaceAll("'", "\\\\'") + "', " // 名字中有单引号
-					+ playerSeason.get(22) + ", "
-					+ "'" + playerSeason.get(1) + "', "
-					+ playerSeason.get(2) + ", "
-					+ playerSeason.get(3) + ", "
-					+ playerSeason.get(4) + ", "
-					+ playerSeason.get(5) + ", "
-					+ playerSeason.get(6) + ", "
-					+ playerSeason.get(7) + ", " 
-					+ playerSeason.get(8) + ", "
-					+ playerSeason.get(9) + ", "
-					+ playerSeason.get(10) + ", "
-					+ playerSeason.get(11) + ", "
-					+ playerSeason.get(12) + ", "
-					+ playerSeason.get(13) + ", "
-					+ playerSeason.get(14) + ", "
-					+ playerSeason.get(15) + ", "
-					+ playerSeason.get(16) + ", "
-					+ playerSeason.get(17) + ", "
-					+ playerSeason.get(18) + ", "
-					+ playerSeason.get(19) + ", "
-					+ playerSeason.get(20)
+					+ matchID + ", "
+					+ "'" + playerMatch.get(21) + "', "
+					+ "'" + playerMatch.get(23) + "', "
+					+ "'" + playerMatch.get(0).replaceAll("'", "\\\\'") + "', " // 名字中有单引号
+					+ playerMatch.get(22) + ", "
+					+ "'" + playerMatch.get(1) + "', "
+					+ playerMatch.get(2) + ", "
+					+ playerMatch.get(3) + ", "
+					+ playerMatch.get(4) + ", "
+					+ playerMatch.get(5) + ", "
+					+ playerMatch.get(6) + ", "
+					+ playerMatch.get(7) + ", " 
+					+ playerMatch.get(8) + ", "
+					+ playerMatch.get(9) + ", "
+					+ playerMatch.get(10) + ", "
+					+ playerMatch.get(11) + ", "
+					+ playerMatch.get(12) + ", "
+					+ playerMatch.get(13) + ", "
+					+ playerMatch.get(14) + ", "
+					+ playerMatch.get(15) + ", "
+					+ playerMatch.get(16) + ", "
+					+ playerMatch.get(17) + ", "
+					+ playerMatch.get(18) + ", "
+					+ playerMatch.get(19) + ", "
+					+ playerMatch.get(20)
 					+ ")";
-		System.out.println(playerSeason);
+		System.out.println(playerMatch);
 		try {
 			Statement statement = mysqlConn.createStatement();
 			statement.execute(sql); // 插入
@@ -382,34 +389,46 @@ public class Season implements NBAData {
 	}
 
 	/**
-	 * 将球队比赛信息保存到team_season表中
+	 * 将球队比赛信息保存到match_profile表中
 	 * @param cellData
 	 * @param regularOrPlayoff 常规赛或者季后赛
 	 * @author cylong
 	 * @version 2015年5月25日  下午3:59:41
 	 */
-	private void insertIntoTeamSeason(ArrayList<String> cellData, String regularOrPlayoff) {
+	private void insertIntoMatchProfile(ArrayList<String> cellData, String regularOrPlayoff) {
 		ArrayList<String> sectionScore = captureSectionAndPlayerData(cellData.get(1)); // 读取每小节分数和球员数据
-		String sql = "INSERT INTO team_season ("
-				+ "gameID, "
+		String date = cellData.get(0); // 将读取下来的date转化成mysql标准格式
+		String splitReg = "[A-Z][a-z]{2}, (?<month>[A-Z][a-z]{2}) (?<day>[0-9]{1,2}), (?<year>[0-9]{4})"; // 拆分抓取下来的时间字符串
+		Pattern patternDate = Pattern.compile(splitReg);
+		Matcher matcherDate = patternDate.matcher(date);
+		String formatDate = null;
+		if(matcherDate.find()) {
+			String year = matcherDate.group("year");
+			String month = this.month.get(matcherDate.group("month"));
+			String day = matcherDate.group("day");
+			formatDate = year + "-" + month + "-" + day;
+		}
+		
+		String sql = "INSERT INTO match_profile ("
+				+ "match_id, "
 				+ "date, "
-				+ "vistor_total_score, "
+				+ "road_total_score, "
 				+ "home_total_score, "
 				+ "section, "
-				+ "visitor, "
-				+ "vistor_section_1, "
-				+ "vistor_section_2, "
-				+ "vistor_section_3, "
-				+ "vistor_section_4, "
-				+ "home, "
+				+ "road_abbr, "
+				+ "road_section_1, "
+				+ "road_section_2, "
+				+ "road_section_3, "
+				+ "road_section_4, "
+				+ "home_abbr, "
 				+ "home_section_1, "
 				+ "home_section_2, "
 				+ "home_section_3, "
 				+ "home_section_4, "
-				+ "regular_or_playoff"
+				+ "season"
 				+ ") VALUES ("
-				+ gameID + ", "
-				+ "'" + cellData.get(0) + "', "
+				+ matchID + ", "
+				+ "'" + formatDate + "', "
 				+ cellData.get(3) + ", "
 				+ cellData.get(5) + ", "
 				+ sectionScore.get(0) + ", "
@@ -426,7 +445,7 @@ public class Season implements NBAData {
 				+ "'" + regularOrPlayoff + "'"
 				+ ")";
 		try {
-			// statement用来执行SQL语句            
+			// statement用来执行SQL语句   
 			Statement statement = mysqlConn.createStatement();
 			statement.execute(sql); // 插入
 		} catch (SQLException e) {
@@ -444,12 +463,12 @@ public class Season implements NBAData {
 		int otNum = extraTime.size() / 2; // 加时赛数
 		for(int i = 0; i < otNum; i++) {
 			String sql = "INSERT INTO extra_time ("
-					+ "gameID, "
-					+ "num, "
-					+ "vistor_score, "
+					+ "match_id, "
+					+ "extra_order, "
+					+ "road_score, "
 					+ "home_score"
 					+ ") VALUES (" 
-						+ gameID + ", "
+						+ matchID + ", "
 						+ (i + 1) + ", "
 						+ extraTime.get(i) + ", "
 						+ extraTime.get(i + otNum)
@@ -490,14 +509,14 @@ public class Season implements NBAData {
     	@Override
     	public void run() {
     		long start = System.currentTimeMillis();
-    		captureSeasonData();
+    		captureMatchData();
     		long stop = System.currentTimeMillis();
     		long millisecond = stop - start;
     		long hour = millisecond / 1000 / 60 / 60;
     		long minute = millisecond / 1000 / 60 % 60;
     		long second = millisecond / 1000 % 60;
     		System.out.println("最近" + maxYear + "年数据抓取完毕！");
-    		System.out.println("共有" + (gameID--) + "场比赛！");
+    		System.out.println("共有" + (matchID--) + "场比赛！");
     		System.out.println("耗时：" + hour + "小时" + minute + "分钟" + second + "秒");
     	}
     }
@@ -529,7 +548,7 @@ public class Season implements NBAData {
 
 		@Override
     	public void run() {
-			insertIntoTeamSeason(cellData, regularOrPlayoff);
+			insertIntoMatchProfile(cellData, regularOrPlayoff);
     	}
     }
     
