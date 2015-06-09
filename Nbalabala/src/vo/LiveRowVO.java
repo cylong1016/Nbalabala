8ec47203e8c4eb640c27e99b2bf19a1c070ec87b
IssacDing <IssacDing@yahoo.com>
2015-06-09 21:32:51 +0800

    同上

--- Nbalabala/src/vo/LiveRowVO.java
+++ Nbalabala/src/vo/LiveRowVO.java
@@ -11,17 +11,16 @@ package vo;
  */
 public class LiveRowVO {
 	
-	
-	/** 时间 */
-	public String time;
-	
+
 	/** 如果这一行是“第一节开始”、“勇士队请求暂停”之类的，这个属性不为null。这时候合并Jtable
 	 * 的后三列，显示这一事件 */
 	public String event;
 	
-	/** 在event为null的时候，后面这三个属性才会被显示。
-	 * 	当event不为null时，这三个属性都是null */
-
+	/** 在event为null的时候，后面这4个属性才会被显示。
+	 * 	当event不为null时，这4个属性都是null */
+	
+	/** 时间 */
+	public String time;
 	/** 客场发生了什么 */	
 	public String roadEvent;
 	/** 两队当前比分 */
