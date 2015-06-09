e3662d4e99fb69c0e19ab79d3c81a6b183a35b3e
IssacDing <IssacDing@yahoo.com>
2015-06-09 20:39:19 +0800

    同上

--- Nbalabala/src/blservice/LiveBLService.java
+++ Nbalabala/src/blservice/LiveBLService.java
@@ -6,6 +6,7 @@ package blservice;
 import java.util.ArrayList;
 
 import po.MatchPlayerPO;
+import vo.LivePlayerVO;
 
 /**
  * 直播界面需要的接口
@@ -41,10 +42,10 @@ public interface LiveBLService {
 	public String getHomeAbbr();
 	
 	/** 获取客场球员的记录，PO里面要有所有页面上能爬到的属性 */
-	public ArrayList<MatchPlayerPO> getRoadPlayerRecords();
+	public ArrayList<LivePlayerVO> getRoadPlayerRecords();
 	
 	/** 获取主场球员的记录，PO里面要有所有页面上能爬到的属性 ，这个网页上没有的属性不用管。注意“篮板”是totalRebounds*/
-	public ArrayList<MatchPlayerPO> getHomePlayerRecords();
+	public ArrayList<LivePlayerVO> getHomePlayerRecords();
 	
 	/** 获取客场各节比分。
 	 * 比如当前进行到第二节，第一节是20，第二节目前15，那么List就是size为2，两个元素是20和15 */
