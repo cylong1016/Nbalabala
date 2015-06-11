012acffe2df4d4eaba66d22566e791dab0667fb1
陈云龙 <cyl19941016@sina.com>
2015-06-12 00:32:38 +0800

    添加刷新数据的接口

--- Nbalabala/src/blservice/LiveBLService.java
+++ Nbalabala/src/blservice/LiveBLService.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package blservice;
 
 import java.util.ArrayList;
@@ -15,6 +12,13 @@ import vo.LivePlayerVO;
  * @version 1.0
  */
 public interface LiveBLService {
+	
+	/**
+	 * 每次调用其它方法的时候就要调用这个方法刷新数据
+	 * @author cylong
+	 * @version 2015年6月12日  上午12:30:04
+	 */
+	public void refresh();
 	
 	/**
 	 * 返回将要或者正在直播的比赛列	表
@@ -49,10 +53,19 @@ public interface LiveBLService {
 	
 	/** 获取客场各节比分。
 	 * 比如当前进行到第二节，第一节是20，第二节目前15，那么List就是size为2，两个元素是20和15 */
+	/** 修改：最后还加上总比分
+	 * @author cylong
+	 * @version 2015年6月12日  上午12:05:30
+	 */
 	public ArrayList<Integer> getRoadScores();
 
 	/** 获取主场各节比分。
 	 * 比如当前进行到第二节，第一节是20，第二节目前15，那么List就是size为2，两个元素是20和15 */
+	/**
+	/** 修改：最后还加上总比分 
+	 * @author cylong
+	 * @version 2015年6月12日  上午12:05:00
+	 */
 	public ArrayList<Integer> getHomeScores();
 	
 	/** 获取当前小节数，如当前第三小节正在进行，返回3 */
