158a0474fe10fbe32f98d9a91daf5111b2e92aba
lsy <864986781@qq.com>
2015-06-14 23:15:03 +0800

    同上

--- Nbalabala/src/ui/panel/gamedata/live/LiveInPanel.java
+++ Nbalabala/src/ui/panel/gamedata/live/LiveInPanel.java
@@ -33,8 +33,8 @@ public class LiveInPanel extends BottomPanel{
 	//06月15日 08：00=季后赛 勇士-骑士, 06月17日 09：00=季后赛 骑士-勇士
 	public LiveInPanel(String url){
 		super(url);
-//		live = new LiveMock();
-		live = new Live();
+		live = new LiveMock();
+//		live = new Live();
 		gameDetail = new MyLabel[6];
 		live.refresh();
 		readHash();
