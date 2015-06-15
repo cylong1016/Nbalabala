278b0499c992f54aa36a16f51d8212388c34854b
陈云龙 <cyl19941016@sina.com>
2015-06-15 08:37:41 +0800

    添加结束比赛判断

--- Nbalabala/src/bl/livebl/Live.java
+++ Nbalabala/src/bl/livebl/Live.java
@@ -404,14 +404,14 @@ public class Live implements LiveBLService {
 			}
 			String pause = pauseMatcher.group("pause");
 			isOver = Pattern.matches(".+?(比赛结束)+.+?", source);
-//			if (isOver) {
-//				textLive.add(sid - 2, pause);
-//			} else {
-//			}
-			if(maxSID - sid >= textLive.size()) {
-				textLive.add(pause);
+			if (isOver) {
+				textLive.add(sid - 2, pause);
 			} else {
-				textLive.add(maxSID - sid, pause);
+				if(maxSID - sid >= textLive.size()) {
+					textLive.add(pause);
+				} else {
+					textLive.add(maxSID - sid, pause);
+				}
 			}
 		}
 	}
