8928b6a5d7b5e50c0aa449ddb54fa3a530552796
陈云龙 <cyl19941016@sina.com>
2015-06-17 05:09:59 +0800

    AdvancedDataService接口添加注释

--- Nbalabala/src/dataservice/AdvancedDataService.java
+++ Nbalabala/src/dataservice/AdvancedDataService.java
@@ -10,7 +10,17 @@ import po.ClutchPO;
  */
 public interface AdvancedDataService {
 
+	/**
+	 * 获得球员高阶数据，包括ORPM DRPM RPM WAR
+	 * @author cylong
+	 * @version 2015年6月17日  上午5:09:10
+	 */
 	public AdvancedDataPO getAdvancedData(String playerName, String season);
 
+	/**
+	 * 获得球员决胜时刻数据，包括上场时间和得分
+	 * @author cylong
+	 * @version 2015年6月17日  上午5:09:21
+	 */
 	public ClutchPO getClutchData(String playerName, String season);
 }
