c24904b452e1cbdc102263b8c3d4f913641c5f66
陈云龙 <cyl19941016@sina.com>
2015-06-16 22:33:21 +0800

    开始写二维图

--- Nbalabala/src/ui/common/jfreechart/Test.java
+++ Nbalabala/src/ui/common/jfreechart/Test.java
@@ -15,6 +15,9 @@ import blservice.AnalysisBLService;
  */
 public class Test extends JFrame{
 	
+	/** serialVersionUID */
+	private static final long serialVersionUID = -2980974327089396371L;
+
 	public Test(){
 		this.setSize(UIConfig.WIDTH, UIConfig.HEIGHT);
 	}
